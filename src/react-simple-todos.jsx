8d05d29d879cd12adc5a94e101dd8e126b4382f1
Matt Robison <bainem@gmail.com>
2015-04-16 16:20:13 -0400

    Add complete and delete UI interactions to tasks.

--- src/react-simple-todos.jsx
+++ src/react-simple-todos.jsx
@@ -2,13 +2,37 @@
   * @jsx React.DOM
   */
 
+
+var TaskItem = React.createClass({
+    handleDeleteClick: function(event) {
+        Tasks.remove(this.props.task._id);
+    },
+
+    handleCheckboxChange: function(event) {
+        Tasks.update(this.props.task._id, {$set: {checked: ! this.props.task.checked}});
+    },
+
+    render: function() {
+        var checkedClass = '';
+        if (this.props.task.checked === true) {
+          checkedClass = "checked";
+        }
+
+        return <li className={checkedClass}>
+                  <button className="delete" onClick={this.handleDeleteClick}>&times;</button>
+                  <input type="checkbox" checked={this.props.task.checked} onChange={this.handleCheckboxChange} />
+                  <span className="text">{this.props.task.text}</span>
+               </li>
+    }
+});
+
 var TaskList = React.createClass({
     render: function() {
         return <ul>
                 {this.props.tasks.map(function(task) {
-                  return <li>{task.text}</li>;
+                  return <TaskItem task={task} key={task._id} />;
                 })}
-               </ul>;
+               </ul>
     }
 });
 
@@ -16,21 +40,21 @@ var TaskApp = React.createClass({
     mixins: [ReactMeteor.Mixin],
 
     getMeteorState: function() {
-      return {
-        tasks: Tasks.find()
-      };
+        return {
+          tasks: Tasks.find()
+        };
     },
 
     handleSubmit: function(event) {
-      var text = event.target.text.value;
+        var text = event.target.text.value;
 
-      Tasks.insert({
-        text: text,
-        createdAt: new Date()
-      });
+        Tasks.insert({
+          text: text,
+          createdAt: new Date()
+        });
 
-      event.target.text.value = '';
-      return event.preventDefault();
+        event.target.text.value = '';
+        return event.preventDefault();
     },
 
     render: function() {
@@ -47,10 +71,10 @@ var TaskApp = React.createClass({
 });
 
 if (Meteor.isClient) {
-  Meteor.startup(function() {
-    Tracker.autorun(function () {
-      React.render(<TaskApp />, document.getElementById("taskApp"));
+    Meteor.startup(function() {
+        Tracker.autorun(function () {
+          React.render(<TaskApp />, document.getElementById("taskApp"));
+        });
     });
-  });
 }
 
